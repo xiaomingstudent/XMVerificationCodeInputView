253322b6b11e40311e6bdd8a664a917ace058a16
minglica <minglica@isoftstone.com>
2020-12-25 16:04:36 +0800

    基础

---
+++ Example/Pods/Target Support Files/Pods-XMVerificationCodeInputView_Tests/Pods-XMVerificationCodeInputView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XMVerificationCodeInputView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XMVerificationCodeInputView_Tests
+@end
---
+++ Example/Pods/Target Support Files/XMVerificationCodeInputView/XMVerificationCodeInputView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XMVerificationCodeInputView : NSObject
+@end
+@implementation PodsDummy_XMVerificationCodeInputView
+@end
